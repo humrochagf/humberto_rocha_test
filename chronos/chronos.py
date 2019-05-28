c50bfcc84d3b9c553d6da9e9a9fef0bcbe6b39e7
Humberto Rocha <humrochagf@gmail.com>
2019-05-28 00:34:22 -0300

    Documenting the class

--- chronos/chronos.py
+++ chronos/chronos.py
@@ -2,8 +2,16 @@ import pendulum
 
 
 class Chronos:
+    """Chronos LRU cache library with time expiration"""
 
     def __init__(self, max_size=100):
+        """Initializes Chronos
+
+        Parameters
+        ----------
+        max_size : int
+            Maximum number of pages to be cached
+        """
         self.head = None
         self.tail = None
         self.map = {}
@@ -62,12 +70,39 @@ class Chronos:
                 return node
 
     def get(self, key):
+        """Gets the cached content by its key
+
+        Parameters
+        ----------
+        key : hashable
+            Any hashable that represents the key
+            of the resource to be retrieved
+
+        Return
+        ------
+        any
+            The content cached
+        """
         node = self._get_node(key)
 
         if node:
             return node.data
 
     def set(self, key, data, expiration=None):
+        """Store content by its key
+
+        Parameters
+        ----------
+        key : hashable
+            Any hashable that represents the key
+            of the resource to be cached
+        data : any
+            The content to be cached
+        expiration : int/float
+            The expiration in seconds. When setted,
+            the cached content will be available
+            until its expiration.
+        """
         node = self._get_node(key)
 
         if node:
@@ -83,6 +118,7 @@ class Chronos:
 
 
 class Node:
+    """The cached page element"""
 
     def __init__(self, key, data, expiration=None):
         self.key = key
